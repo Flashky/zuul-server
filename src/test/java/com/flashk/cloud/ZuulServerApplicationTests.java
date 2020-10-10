ced727009b1cc876c6d6d169686ea08c59833bc6
Flashk <mevengar@gmail.com>
2020-10-10 09:32:27 +0200

    Initial commit

---
+++ src/test/java/com/flashk/cloud/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashk.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
